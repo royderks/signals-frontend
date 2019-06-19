9aff77fa88399626e93273654b909d3a93b2c7bc
Jasper Swart <jasper.g.swart@gmail.com>
2019-06-19 14:52:34 +0200

    [SIG-1373] added unit test for History and Location

--- src/signals/incident-management/containers/IncidentDetail/components/Detail/components/Location/index.test.js
+++ src/signals/incident-management/containers/IncidentDetail/components/Detail/components/Location/index.test.js
@@ -1,10 +1,75 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import Location from 'index';
+import stadsdeelList from 'signals/incident-management/definitions/stadsdeelList';
+
+import Location from './index';
+
+jest.mock('../../../MapDetail', () => () => 'MapDetail');
 
 describe('<Location />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(true);
+  let props;
+
+  beforeEach(() => {
+    props = {
+      incident: {
+        location: {
+          extra_properties: null,
+          geometrie: {
+            type: 'Point',
+            coordinates: [
+              4.913291931152344,
+              52.36582256756977
+            ]
+          },
+          buurt_code: 'A08d',
+          created_by: null,
+          address: {
+            postcode: '1018DH',
+            huisletter: '',
+            huisnummer: '48',
+            woonplaats: 'Amsterdam',
+            openbare_ruimte: 'Plantage Middenlaan',
+            huisnummer_toevoeging: ''
+          },
+          stadsdeel: 'A',
+          bag_validated: false,
+          address_text: 'Plantage Middenlaan 48 1018DH Amsterdam',
+          id: 3158
+        }
+      },
+      stadsdeelList,
+      onShowLocation: jest.fn(),
+      onEditLocation: jest.fn()
+    };
+  });
+
+  it('should render correctly with address', () => {
+    const wrapper = shallow(
+      <Location {...props} />
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render correctly without address (only geo)', () => {
+    props.incident.location.address = undefined;
+    props.incident.location.address_text = undefined;
+    const wrapper = shallow(
+      <Location {...props} />
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render correctly with address and huisletter and huisnummer_toevoeging', () => {
+    props.incident.location.address.huisletter = 'A';
+    props.incident.location.address.huisnummer_toevoeging = '3';
+
+    const wrapper = shallow(
+      <Location {...props} />
+    );
+
+    expect(wrapper).toMatchSnapshot();
   });
 });
--- src/signals/incident-management/containers/IncidentDetail/components/History/index.test.js
+++ src/signals/incident-management/containers/IncidentDetail/components/History/index.test.js
@@ -1,10 +1,46 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import History from 'index';
+import History from './index';
 
 describe('<History />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(true);
+  it('should render correctly', () => {
+    const props = {
+      list: [
+        {
+          identifier: 'UPDATE_STATUS_6146',
+          when: '2019-06-19T14:02:52.048970+02:00',
+          what: 'UPDATE_STATUS',
+          action: 'Update status naar: Ingepland',
+          description: null,
+          who: 's.l.kok@amsterdam.nl',
+          _signal: 2934
+        },
+        {
+          identifier: 'UPDATE_STATUS_6145',
+          when: '2019-06-19T13:16:18.772967+02:00',
+          what: 'UPDATE_STATUS',
+          action: 'Update status naar: Ingepland',
+          description: 'Voor het oplossen van uw melding hebben we helaas wat meer tijd nodig.\n\nDit kan een aantal oorzaken hebben:\no Als er door de aannemer gegraven moet worden, dient er eerst een bodemonderzoek te worden uitgevoerd. \no Soms moet er extra werk en aanvullende maatregelen worden ingepland. Bijvoorbeeld de verlichting boven een trambaan, dat samen met het GVB moet worden hersteld.\no Er moet materiaal besteld worden dat niet op voorraad is.\n\nU ontvangt bericht wanneer we uw melding hebben afgerond.\n\nU kunt op werkdagen tijdens kantooruren ook bellen naar telefoonnummer 020 254 5216. Heeft u een vraag over uw melding, vermeld dan het meldingsnummer.',
+          who: 'sia@techtek.eu',
+          _signal: 2934
+        },
+        {
+          identifier: 'UPDATE_STATUS_6144',
+          when: '2019-06-19T13:16:18.508042+02:00',
+          what: 'UPDATE_STATUS',
+          action: 'Update status naar: In afwachting van behandeling',
+          description: '',
+          who: 'sia@techtek.eu',
+          _signal: 2934
+        }
+      ]
+    };
+
+    const wrapper = shallow(
+      <History {...props} />
+    );
+
+    expect(wrapper).toMatchSnapshot();
   });
 });
